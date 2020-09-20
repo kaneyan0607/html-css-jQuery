ae2e6d1587f5782fb1ad4ca8620c339adc16d277
kaneyan0607 <kaneyan0607@gmail.com>
2020-09-21 04:30:17 +0900

    add:ハンバーガーメニューの実装

--- html-kaguya/script.js
+++ html-kaguya/script.js
@@ -1,11 +1,26 @@
+//各ページへ遷移したときにフェードイン
 $(function () {
-    $("main, footer").fadeIn(500); //各ページへ遷移したときにフェードイン
+    $("main, footer").fadeIn(500);
 });
 
-//ハンバーガー
+//ハンバーガーアイコンとドロワーメニュー
 $(function () {
     $('.btn-trigger').on('click', function () {
         $(this).toggleClass('active');
-        return false;
+
+        if ($(this).hasClass('active')) {
+            $('.nav-media').addClass('active');
+            $("header").addClass('shadow-active');
+        } else {
+            $('.nav-media').removeClass('active');
+        }
     });
 });
+
+//ヘッダーの高さ分だけコンテンツを下げる
+if (matchMedia('(max-width: 375px)').matches) {
+    $(function () {
+        const height = $("header").height();
+        $("main").css("padding-top", height);
+    });
+}
