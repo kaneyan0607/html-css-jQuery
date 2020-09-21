16c55e3260da4c16f558c9e2571e3ac83801ea83
kaneyan0607 <kaneyan0607@gmail.com>
2020-09-22 02:19:27 +0900

    add:ハンバーガーメニュにて遷移時のドロワーメニューアニメーションを追加 fix:コメントアウトを一部修正（ハンバーガーメニューをドロワーメニューに修正）

--- html-kaguya/script.js
+++ html-kaguya/script.js
@@ -13,11 +13,31 @@ $(function () {
             $("header").addClass('shadow-active');
         } else {
             $('.nav-media').removeClass('active');
+            $("header").removeClass('shadow-active');
         }
     });
 });
 
-//ヘッダーの高さ分だけコンテンツを下げる
+//ページ遷移の際、ドロワーメニューが閉じるアニメーション
+$(function () {
+    $('.nav-media a').on('click', function () {
+        $('.nav-media').toggleClass('active');
+        $(".btn-trigger").toggleClass('active');
+        $("header").removeClass('shadow-active');
+    })
+});
+
+//ページ推移時に約0.3秒後にリンク先へ遷移
+$("a").on('click', function (event) {
+    event.preventDefault();
+    var linkUrl = $(this).attr('href');
+    function action() {
+        location.href = linkUrl;
+    }
+    setTimeout(action, 280);
+});
+
+//レスポンス時、ヘッダーの高さ分だけmainを下げる
 if (matchMedia('(max-width: 375px)').matches) {
     $(function () {
         const height = $("header").height();
